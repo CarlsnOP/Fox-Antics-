278d7d41a4260befe666b931b4be42fd90f86b55
CarlsnOP <Anderscarlsen94@gmail.com>
2024-06-11 09:34:27 +0300

    course step 138
    Player can now be hit and enter invincible mode and hurt mode

--- Player/player.gd
+++ Player/player.gd
@@ -1,28 +1,35 @@
 extends CharacterBody2D
 
+
 class_name Player
+
 
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
 @onready var debug_label = $DebugLabel
 @onready var sound_player = $SoundPlayer
 @onready var shooter = $Shooter
+@onready var animation_player_invincible = $AnimationPlayerInvincible
+@onready var invincible_timer = $InvincibleTimer
+@onready var hurt_timer = $HurtTimer
 
 
 const GRAVITY: float = 1000.0
 const RUN_SPEED: float = 120.0
 const MAX_FALL: float = 400.0
-const HURT_TIME: float = 0.3
 const JUMP_VELOCITY: float = -400.0
-const DASH_SPEED: float = 900.0
+const _HURT_JUMP_VELOCITY: Vector2 = Vector2(0, -150.0)
+
 
 enum PLAYER_STATE { IDLE, RUN, JUMP, FALL, HURT, DASH }
 
+
 var _state: PLAYER_STATE = PLAYER_STATE.IDLE
+var _invincible: bool = false
+
 
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -37,10 +44,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot") == true:
 		shoot()
 
-
 func update_debug_label() -> void:
-	debug_label.text = "floor: %s\n%s\n%.0f,%.0f" % [
-		is_on_floor(),
+	debug_label.text = "floor: %s inv:%s\n%s\n%.0f,%.0f" % [
+		is_on_floor(), _invincible,
 		PLAYER_STATE.keys()[_state],
 		velocity.x, velocity.y
 	]
@@ -52,7 +58,9 @@ func shoot() -> void:
 		shooter.shoot(Vector2.RIGHT)
 
 func get_input() -> void:
-	
+	if _state == PLAYER_STATE.HURT:
+		return
+		
 	velocity.x = 0
 	
 	if Input.is_action_pressed("left") == true:
@@ -100,8 +108,31 @@ func set_state(new_state: PLAYER_STATE) -> void:
 		PLAYER_STATE.FALL:
 			animation_player.play("fall")
 
+func apply_hurt_jump() -> void:
+	set_state(PLAYER_STATE.HURT)
+	animation_player.play("hurt")
+	velocity = _HURT_JUMP_VELOCITY
+	hurt_timer.start()
+
+func go_invincible() -> void:
+	_invincible = true
+	animation_player_invincible.play("invincible")
+	invincible_timer.start()
+
+func apply_hit() -> void:
+	if _invincible == true:
+		return
+		
+	go_invincible()
+	apply_hurt_jump()
+	SoundManager.play_clip(sound_player, SoundManager.SOUND_DAMAGE)
+
 func _on_hit_box_area_entered(area):
-	pass # Replace with function body.
+	apply_hit()
 
+func _on_invincible_timer_timeout():
+	_invincible = false
+	animation_player_invincible.stop()
 
-
+func _on_hurt_timer_timeout():
+	set_state(PLAYER_STATE.IDLE)
